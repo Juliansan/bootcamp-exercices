239bd4f20040128e4c90863148d933ffd7530cfc
Julian Sanjuan <Julsanm@gmail.com>
2022-07-16 13:17:07 +0200

    RandomInt Finder game

---
+++ src/com/ironhack/bootcamp/exercices/s2/FindRandomInt.java
@@ -0,0 +1,34 @@
+package com.ironhack.bootcamp.exercices.s2;
+
+import java.util.Scanner;
+
+public class FindRandomInt {
+    public static void main(String[] args) {
+
+        int number = (int)(Math.random()*10);
+        int tries = 5;
+        boolean numberFound = false;
+        Scanner sc = new Scanner(System.in);
+
+
+        int inputNumber;
+        while (tries >= 0) {
+            System.out.println("Input an Integer: ");
+            inputNumber = sc.nextInt();
+
+            if (inputNumber == number) {
+                numberFound = true;
+                break;
+            }
+            tries--;
+        }
+        sc.close();
+        if (numberFound) {
+            System.out.println("Your number is correct: " + number);
+            System.out.println("Congrats");
+        } else {
+            System.out.println("Sorry the number was: " + number);
+            System.out.println("Next Time!");
+        }
+    }
+}
